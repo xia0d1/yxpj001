3d2dcc2b8293faf5ed1c5df49ac886746adc5c91
JIE-yx <1219722384@qq.com>
2021-12-31 10:31:11 +0800

    add new branch

--- src/main/java/com/yx/test/demo.java
+++ src/main/java/com/yx/test/demo.java
@@ -6,9 +6,9 @@ public class demo {
 
 
         System.out.println("这是主分支");
-        System.out.println("这是颜信的开发分支");
+        System.out.println("这是潘杰的开发分支");
 
-        System.out.println("测试commit");
+
 
 
     }
